db73de63b48cabf0dd7213fc3dfaeac5539ed77e
ITSAIDI <noureddinesaidi111@gmail.com>
2024-08-23 16:30:06 +0100

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import Main from "./Componenets/Main";
 import SideBar from "./Componenets/SideBar";
 import Footer from "./Componenets/Footer";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App() {
 
-    const NASA_API = import.meta.env.NASA_API_KEY;
 
     const [ShowSidebar,setShowSidebar] = useState(false)
+    const [Data,setData]=useState(null)
+    const [Loading,setLoading]=useState(false)
+
+    useEffect(()=>{
+        async function FetchImage(params) {
+            const NASA_KEY = import.meta.env.NASA_API_KEY;
+            const url = 'https://api.nasa.gov/planetary/apod'+`?api_key=${"DyqgIQPtyglUGTxZmXhFd4XYzNYKuJjwcXdx3Wwu"}`
+            try {
+                const res = await fetch(url)
+                const apidata = await res.json()
+                setData(apidata)
+                console.log('DATA \n',apidata)
+                
+            } catch (error) {
+                console.log(error.message)
+                
+            }
+            
+        }
+        //FetchImage()
+
+    },[])
 
     function handleToggleSidebar ()
     {
@@ -16,12 +37,14 @@ function App() {
 
     return (
         <div id="root">
-                <Main />
-                {
-                    ShowSidebar && (<SideBar handleToggleSidebar_S = {handleToggleSidebar} /> )
-                }
-                <Footer handleToggleSidebar_F = {handleToggleSidebar}/>
-                </div>
+            {
+                Data ? ( <Main />) : <div className="loading"> <i className="fa-solid fa-gear"></i></div>
+            }
+            {
+                ShowSidebar && (<SideBar handleToggleSidebar_S = {handleToggleSidebar} /> )
+            }
+            <Footer handleToggleSidebar_F = {handleToggleSidebar}/>
+            </div>
 
     );
 }
--- src/Componenets/Footer.jsx
+++ src/Componenets/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(props) {
     return (
         <footer>
             <div className="GradientBackground"></div>
-            <h3>Our beautiful Space Subhana Allah
+            <h3>APOD (Astronomy picture of the day) 
              <h4>Copyright &copy; {new Date().getFullYear()}</h4>
              <button style={{backgroundColor: "transparent", border: "none"}} onClick={handleToggleSidebar_F}>
                 <i className="fa-solid fa-circle-info" style={{color: "white"}}></i>
